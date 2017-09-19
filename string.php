8287ab68e1c39f8dd4188a29c70d10a9582d43bf
codingfish <wangliang199538@live.com>
2017-09-19 15:24:49 +0800

    回顾了一下字符串类型

--- string.php
+++ string.php
@@ -10,12 +10,12 @@ $redis = new Redis();
 
 $redis->connect('localhost');
 
-$info = $redis->set('test', 123);
+//设置字符串，第一个参数key,第二个参数value。设置成功返回1
+$info = $redis->set('test', 'hello redis');
+echo $info.PHP_EOL;
 
-echo $info;
+//获取对应key的值，没有返回空
+echo $redis->get('test').PHP_EOL;
 
-echo $redis->get('test');
-
-echo PHP_EOL;
-
-$redis->del('test');
+//删除对应的键值对，返回受影响的行数
+echo $redis->del('test').PHP_EOL;
